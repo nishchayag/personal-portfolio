4bcb382d63e1620a1b3b28f9ad388e60b6fadfac
Nishchay Agarwal <nishchay.agar@gmail.com>
2025-06-13 15:51:32 +0530

    more

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,16 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+{
+  "rules": {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_"
+      }
+    ]
+  }
+}
+
+
 export default eslintConfig;
