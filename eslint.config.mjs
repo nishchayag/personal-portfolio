58415a729bf25a24842055dcd9fb88748a9112b2
Nishchay Agarwal <nishchay.agar@gmail.com>
2026-03-29 09:58:26 +0530

    nextjs update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // ⬇️ custom overrides go here
+export default defineConfig([
+  ...nextVitals,
+  ...nextTypescript,
   {
     rules: {
-      // Ignore unused function parameters whose name starts with “_”
       "@typescript-eslint/no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_" },
       ],
     },
   },
-];
-
-export default eslintConfig;
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
